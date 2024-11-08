d75c76ddf17f8b98cd28a02369e3b0b002f85ff7
TonnyStarkkk <aidar20031982@gmail.com>
2024-11-08 18:27:25 +0600

    Commit123

--- app/src/main/java/com/example/home_work2_7th_mon/domain/repository/TaskRepository.kt
+++ app/src/main/java/com/example/home_work2_7th_mon/domain/repository/TaskRepository.kt
@@ -3,8 +3,8 @@ package com.example.home_work2_7th_mon.domain.repository
 import com.example.home_work2_7th_mon.data.model.TaskModel
 
 interface TaskRepository {
-    fun addTask(taskModel: TaskModel)
-    fun update(taskModel: TaskModel)
+    suspend fun addTask(taskModel: TaskModel)
+    suspend fun update(taskModel: TaskModel)
     fun delete(taskModel: TaskModel)
     fun getById(id: Int): TaskModel
     fun getAll(): List<TaskModel>
